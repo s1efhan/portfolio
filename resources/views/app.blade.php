2e901f7c7625857a487ae1a87e441b6613608acd
s1efhahn <stefan.theissen@mail.de>
2024-02-12 00:04:59 +0100

    changing favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="de">
+  
 <head> 
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" href="/favicon.ico">
         @vite('resources/css/style.scss')
         @vite('resources/js/app.js')
 </head>
