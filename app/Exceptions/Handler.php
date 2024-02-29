245f5b71e55762e7ffe145410c21d4b15f9aedfa
s1efhahn <stefan.theissen@mail.de>
2024-02-29 21:54:59 +0100

    neues wartungsarbeiten schild

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($this->isHttpException($exception) && $exception->getStatusCode() == 503) {
+            return response()->view('errors.custom503', [], 503);
+        }
+
+        return parent::render($request, $exception);
     }
 }
---
+++ resources/views/errors/custom503.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="initial-scale=1.0, width=device-width">
+  <link rel="icon" href="/favicon.ico">
+  @vite('resources/css/style.scss')
+  @vite('resources/js/app.js')
+  <style> body {display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; width: 100vw;
+            margin: 0;}</style>
+</head>
+
+<body>
+    <div class="wartungsarbeiten-container">
+<h2>Wartungsarbeiten</h2>
+<h3>503 - SERVICE UNAVAILABLE </h3>
+<p>Currently under construction </p>
+</div>
+<div class ="wartungsarbeiten-hintergrund"> <h2>Wartungsarbeiten</h2>
+<h3>503 - SERVICE UNAVAILABLE </h3>
+<p>Currently under construction </p><div>
+</body>
+
+</html>
